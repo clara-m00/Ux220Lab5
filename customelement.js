78f45dd25cd87abdee06fa486dc89f1a48b7c0c7
clara-m00 <claramend20@gmail.com>
2023-11-08 20:46:14 -0500

    added in date element

--- customelement.js
+++ customelement.js
@@ -1,16 +1,23 @@
-class ClaraElement extends HTMLElement{
-    connectedCallback(){
+class ClaraElement extends HTMLElement {
+    connectedCallback() {
         this.innerHTML = "Clara was here";
     }
 }
 
 customElements.define("x-clara", ClaraElement);
 
-class CenterElement extends HTMLElement{
-    connectedCallback(){
-        this.innerHTML = `<div style="text-align:center>${this.innerHTML}</div>
-        `
+class CenterElement extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `<div style="text-align:center">${this.innerHTML}</div>`;
     }
 }
 
 customElements.define("x-center", CenterElement);
+
+class YearElement extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = new Date().getFullYear();
+    }
+}
+
+customElements.define("x-fullyear", YearElement);
